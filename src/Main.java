2d54a4dedcbf3f1373241ac09a0a10b171002e78
darcyp <eelakoopfm@gmail.com>
2023-02-01 18:07:54 +0200

    Решила 4ю задачу

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
 
     }
 
@@ -39,6 +40,23 @@ public class Main {
 
     public static void task4(){
         System.out.println("Задача 4");
+        int age = 10;
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад.");
+        } else {
+            if (age >= 7 && age <= 18) {
+                System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу.");
+            } else {
+                if (age > 18 && age <= 24) {
+                    System.out.println("Если возраст человека равен " + age + ", то его место в университете.");
+                } else {
+                    if (age >24) {
+                        System.out.println("Если возраст человека равен " + age + ", то ему пора ходить на работу.");
+                    }
+                }
+
+                }
+            }
     }
 
     public static void task5() {
