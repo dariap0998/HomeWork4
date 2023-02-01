7cb0ad3592c8176b6eb50515bc72e170b156cb13
darcyp <eelakoopfm@gmail.com>
2023-02-01 18:49:30 +0200

    Решила 7ю задачу

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
+        task7();
 
     }
 
@@ -61,8 +63,71 @@ public class Main {
 
     public static void task5() {
         System.out.println("Задача 5");
+        int age = 13;
+        if (age < 5) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе.");
+        } else {
+            if (age >= 5 && age < 14) {
+                System.out.println("Если возраст ребенка равен " + age + ", он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя.");
+            } else {
+                if (age >= 14) {
+                    System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе без сопровождения взрослого.");
+                }
+            }
+        }
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        int trainLoad = 60;
+        if (trainLoad < 60) {
+            System.out.println("В настоящий момент в вагоне занято " + trainLoad + " мест, вы можете занять сидячие место.");
+        } else {
+            if (trainLoad >= 60 && trainLoad < 102) {
+                System.out.println("В настоящий момент в вагоне занято " + trainLoad + " места, вы можете занять стоячее место.");
+            } else {
+                if (trainLoad >= 102) {
+                    System.out.println("В настоящий момент вагон полностью забит.");
+                }
+            }
+        }
+    }
 
+    public static void task7() {
+        System.out.println("Задача 7");
+        int one = 2;
+        int two = 2;
+        int three = 1;
+        if (one > two && one > three) {
+            System.out.println("Число " + one + " бОльшее.");
+        } else {
+            if (two > one && two > three) {
+                System.out.println("Число " + two + " бОльшее.");
+            } else {
+                if (three > one && three > two) {
+                    System.out.println("Число " + three + " бОльшее.");
+                } else {
+                    if (one == two && one > three) {
+                        System.out.println("Число " + one + " бОльшее. И, кстати, оно совпадает со вторым числом.");
+                    } else {
+                        if (one == three && one > two) {
+                            System.out.println("Число " + one + " бОльшее. И, кстати, оно совпадает с третьим числом.");
+                        } else {
+                            if (two == three && three > one) {
+                                System.out.println("Число " + three + " бОльшее. И, кстати, оно совпадает со вторым числом.");
+                            } else {
+                                if (one == two && one == three && three == two) {
+                                    System.out.println("Нет бОльшего числа. Они все одинаковы!");
+                                }
+                            }
+                        }
+
+                }
+            }
+        }
     }
 }
+
+}
+
 
 
